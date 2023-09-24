4acb493504ead8094cb9c22bbb3ef003f295bb84
Nico <112582889+Metioflar@users.noreply.github.com>
2023-09-23 21:00:02 -0300

    Add files via upload

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,21 +1,30 @@
 import { Text, View, TextInput, TouchableOpacity } from "react-native"
 import { styles } from "./styles"
 import { Ionicons } from "@expo/vector-icons"
+import { useState } from "react"
 
 export function HomeScreen() {
-    const toDoList =[
+    const [toDoValue, setToDoValue] =useState("")
+    const [toDoList, setToDoList] = useState([
         {name:"PHP"},
         {name:"java"},
         {name:"Javascript"},
         {name:"Python"}
-    ]
+    ])
+    function addItem(){
+      setToDoList((prev) => {
+        return [...prev, {name: toDoValue}]
+      })
+    }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Lista de Tarefas</Text>
 
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Nome da tarefa" placeholderTextColor="#71717A" style={styles.input} />
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TextInput value={toDoValue} onChangeText={(text)=> setToDoValue(text)} placeholder="Nome da tarefa" placeholderTextColor="#71717A" style={styles.input} />
+        <TouchableOpacity style={styles.buttonAdd}> onPress={() =>{
+          addItem()
+        }}
             <Ionicons name='add-circle-outline' size={32} color='#fff'/>
         </TouchableOpacity>
       </View>
